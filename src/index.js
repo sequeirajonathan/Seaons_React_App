cb48104b665b7dbf0852197d0e7150cb660014fc
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-02-04 22:07:55 -0500

    Created logic and Styling for conditional components

--- src/index.js
+++ src/index.js
@@ -1,43 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
-    // Initialize State
-    constructor(props) {
-        super(props);
-        // THIS IS THE ONLY TIME we do direct assignemnt 
-        // to this.state
-        this.state = { lat: null, errorMessage: '' };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(position => {
+      // we called setState!!!!
+      this.setState({lat: position.coords.latitude});
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState!!!!
-                this.setState({ lat: position.coords.latitude });
+      // we did not!!
+      // this.state.lat = position.coords.latitude
+    }, err => {
+      this.setState({errorMessage: err.message});
+    });
+  }
 
-                // we did not!!
-                // this.state.lat = position.coords.latitude
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
-        );
+  // React says we have to define render!!
+  render() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
 
-    // React says we have to define render!!
-    render() {
-        if(!this.state.lat && this.state.errorMessage){
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if(this.state.lat && !this.state.errorMessage){
-            return <div>Latitude: {this.state.lat}</div>
-        }
-
-        return <div>Loading...</div>
-    };
+    return <div>Loading...</div>;
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector("#root"));
